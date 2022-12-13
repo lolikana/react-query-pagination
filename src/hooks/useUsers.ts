78f5f75f1f8ff8b57d6943e05f807c82bedb99bd
Lolikana <thomas.guiart@gmail.com>
2022-12-13 10:59:28 +0900

    refactoring

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,22 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-// import { TUsers } from '@/libs/types';
+import { TUsers } from '@/libs/types';
 
-// export const getUsers = async (): Promise<TUsers> => {
-//   const res = await axios.get('https://reqres.in/api/users?page=1');
-//   const { data } = res;
-//   return data;
-// };
-
-// export const useUsers = () => {
-//   return useQuery({ queryKey: ['users'], queryFn: () => getUsers() });
-// };
+export const getUsers = async (): Promise<TUsers> => {
+  const res = await axios.get('https://reqres.in/api/users?page=1');
+  const { data } = res;
+  return data;
+};
 
 export const useUsers = () => {
-  return useQuery(['users'], async () => {
-    const { data } = await axios.get('https://reqres.in/api/users?page=1');
-
-    return data;
-  });
+  return useQuery({ queryKey: ['users'], queryFn: () => getUsers() });
 };
